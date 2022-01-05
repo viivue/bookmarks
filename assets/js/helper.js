919dab65fa7da6c89a6f9f2f663cc47527ff1ce3
phucbm <hiphucbui@gmail.com>
2022-01-05 16:04:31 +0700

    feat(api): get labels and issues

---
+++ assets/js/helper.js
@@ -0,0 +1,35 @@
+/**
+ * Get contrast text color base on background color
+ * https://www.w3.org/TR/AERT/#color-contrast
+ * @param rgb
+ * @returns {string}
+ */
+function getTextColor(rgb){
+    // Randomly update colours
+    // rgb[0] = Math.round(Math.random() * 255);
+    // rgb[1] = Math.round(Math.random() * 255);
+    // rgb[2] = Math.round(Math.random() * 255);
+
+    // http://www.w3.org/TR/AERT#color-contrast
+    const brightness = Math.round(((parseInt(rgb[0]) * 299) +
+        (parseInt(rgb[1]) * 587) +
+        (parseInt(rgb[2]) * 114)) / 1000);
+
+    return (brightness > 125) ? '#000' : '#fff';
+}
+
+
+/**
+ * Hex to RGB
+ * https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+ * @param hex
+ * @returns {{r: number, b: number, g: number}|null}
+ */
+function hexToRgb(hex){
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,74 @@
-const DEV = true;
-const token = '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';
-const query = {
-    "url": 'https://api.github.com/repos/viivue/bookmarks/issues',
-    headers: {"authorization": `token ${token}`},
-    "method": "GET",
-    "timeout": 0,
-    "error": (response, status) => {
-        if(DEV) console.log('requestGitlabAPI', query, response, status);
-    }
-};
+jQuery(function($){
+    const DEV = true;
 
-$.ajax(query).done((response, status) => {
-    const $main = $('main');
-    for(const item of response){
-        const color = item.labels[0].color;
-        let labelsHTML = '';
-        for(const label of item.labels){
-            labelsHTML += `<li>${label.name}</li>`;
+    class Bookmark{
+        constructor(owner, repo){
+            this.token = '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';
+            this.apiURL = `https://api.github.com/repos/${owner}/${repo}/`;
+            this.bookmarkContainer = $('[data-container]');
+            this.labelContainer = $('[data-labels]');
+
+            // get label
+            this.getLabels(response => {
+                for(const label of response){
+                    const color = hexToRgb(`#${label.color}`);
+
+                    this.labelContainer.append(`
+                       <li>
+                       <a href="#" class="label" style="--label-r:${color.r};--label-g:${color.g};--label-b:${color.b};">${label.name}</a>
+                       </li>
+                       `);
+                }
+            });
+
+            // get issue
+            this.getIssues('', response => {
+                for(const issue of response){
+                    this.bookmarkContainer.append(this.getBookmarkItemHTML(issue));
+                }
+            });
         }
 
-        $main.append(`
-            <article style="background-color:#${color}">
-            <h2>${item.title}</h2>
-            <div>${item.body}</div>
-            <ul>${labelsHTML}</ul>
-            </article>
-        `);
-        console.log(item)
+        getBookmarkItemHTML(issueObject){
+            const url = new URL(issueObject.title);
+            const title = url.host.replace('www.', '');
+
+            return `
+                <li>
+                <a href="${issueObject.title}" target="_blank" class="btn_underline">${title}</a>
+                <div>${issueObject.body}</div>
+                </li>
+            `;
+        }
+
+        getQueryObject(string){
+            const url = `${this.apiURL}${string}`;
+            const query = {
+                "url": url, "method": "GET", "timeout": 0, "error": (response, status) => {
+                    if(DEV) console.log('getQuery', url, response, status);
+                }
+            };
+
+            // with token
+            if(this.token.length){
+                query['headers'] = {"authorization": `token ${this.token}`};
+            }
+
+            return query;
+        }
+
+        getIssues(labels = '', callback){
+            $.ajax(this.getQueryObject(`issues?state=closed&labels=${labels}`)).done((response, status) => {
+                callback(response);
+            });
+        }
+
+        getLabels(callback){
+            $.ajax(this.getQueryObject(`labels`)).done((response, status) => {
+                callback(response);
+            });
+        }
     }
+
+    const bookmark = new Bookmark('viivue', 'bookmarks');
 });
